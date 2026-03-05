1e41cf02036cf2b60e4c5d49bf1a326417295670
mahmudulhasanmaruf78@gmail.com <mahmudulhasanmaruf78@gmail.com>
2026-03-06 02:26:36 +0600

    add form validation function and update form attributes

---
+++ Lab Practice/Script.js
@@ -0,0 +1,7 @@
+function validateForm() {
+    let firstName = document.getElementById("firstname").value;
+    let lastName = document.getElementById("lastname").value;
+    let email = document.getElementById("email").value;
+    let message = document.getElementById("message").value;
+    let phone = document.getElementById("phone").value;
+}
